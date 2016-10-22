8f5ad2c0cdbc4e0d5ead99be74e3251bc92b2f34
fengyun2 <2816708306@qq.com>
2016-10-22 16:10:57 +0800

    update brand module

--- src/containers/Brand/index.js
+++ src/containers/Brand/index.js
@@ -2,7 +2,7 @@
 * @Author: fengyun2
 * @Date:   2016-10-22 00:50:38
 * @Last Modified by:   fengyun2
-* @Last Modified time: 2016-10-22 14:11:22
+* @Last Modified time: 2016-10-22 16:09:25
 */
 
 import React, {Component} from 'react'
@@ -25,7 +25,21 @@ class Brand extends Component {
     /* console.log(`this.props: `, this.props) */
 
     return (
-      <div className="content-block">这里是内容区</div>
+      <div className="content-block">
+        <div className="content-block-title">分类</div>
+        <div className="card">
+          <div className="card-header color-white no-border no-padding">
+            <img className='card-cover' src="//gqianniu.alicdn.com/bao/uploaded/i4//tfscom/i3/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg" alt="" />
+          </div>
+          <div className="card-content">
+            <div className="card-content-inner">
+              <p className="color-gray">发表于 2016/10/22</p>
+              <p>此处省略一百万字...</p>
+            </div>
+          </div>
+          <div className="card-footer">卡脚</div>
+        </div>
+      </div>
     )
   }
 }
